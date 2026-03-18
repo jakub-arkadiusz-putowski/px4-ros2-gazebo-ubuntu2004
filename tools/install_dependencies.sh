31554136d1f3249f6f1bce9a2cfa2b655ed54e06
jakub-arkadiusz-putowski <jakputowski@gmail.com>
2026-03-18 13:31:44 +0100

    Create install_dependencies.sh

---
+++ tools/install_dependencies.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+source /etc/os-release
+if [[ "$VERSION_ID" != "20.04" ]];then
+  echo "wymaga ub20.04"
+  exit 1
+fi
+
+sudo apt-get update
+sudo apt-get install -y \
+  software-properties-common \
+  curl \
+  git \
+  wget \
+  python3-pip \
+  python3-venv \
+  make \
+  gcc \
+  g++ \
+  build essential
+
+echo "zainstalowano zaleznosci"
