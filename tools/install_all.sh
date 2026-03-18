ccd7b9bf60fbddf8087e4f165987a39c12015a3e
jakub-arkadiusz-putowski <jakputowski@gmail.com>
2026-03-18 13:39:40 +0100

    Update install_all.sh

--- tools/install_all.sh
+++ tools/install_all.sh
@@ -5,9 +5,9 @@ REPO_NAME="px4-ros2-gazebo-ubuntu2004"
 REPO_OWNER="jakub-arkadiusz-putowski"
 REPO_BRANCH="main"
 REPO_URL="https://github.com/${REPO_OWNER}/${REPO_NAME}.git"
-INSTALl_DIR="${HOME}/${REPO_NAME}"
+INSTALL_DIR="${HOME}/${REPO_NAME}"
 
-PX4_DIR="${INSTAL_DIR}/PX4-Autopilot"
+PX4_DIR="${INSTALL_DIR}/PX4-Autopilot"
 PX4_REPO="https://github.com/PX4/PX4-Autopilot.git"
 
 ROS_DISTRO="foxy"
@@ -18,7 +18,7 @@ if [ ! -d "$INSTALL_DIR" ]; then
   git clone "$REPO_URL" "$INSTALL_DIR"
 else
   echo "aktualizacja '$REPO_NAME'"
-  cd "$REPO_NAME" && git pull origin main
+  cd "$INSTALL_DIR" && git pull origin main
 fi
 
 
