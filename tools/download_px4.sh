567cefff260a56e45da560f094368428b191db31
jakub-arkadiusz-putowski <jakputowski@gmail.com>
2026-03-18 13:42:16 +0100

    Update download_px4.sh

--- tools/download_px4.sh
+++ tools/download_px4.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PX4_DIR="$HOME/px4ros2-gz-ub2004/PX4-Autopilot"
+PX4_DIR="$HOME/px4-ros2-gazebo-ub2004/PX4-Autopilot"
 
-if [ ! -d "PX4_DIR" ]; then
+if [ ! -d "$PX4_DIR" ]; then
   echo "klonuje px4"
-  git clone https://github.com/PX4/PX4-Autopilot.git --recursive "$PX4_DIR"
+  git clone https://github.com/PX4/PX4-Autopilot.git --branch v1.14.3 --recursive "$PX4_DIR"
 else
   echo "px4 zainstalowane w $PX4_DIR"
 fi
